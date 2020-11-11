a5cc278c5bdfebe98e72314aded753c135974c5b
neil <win10@neilpang.com>
2020-11-11 22:52:32 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# ngrok-tunnel
-ngrok tunnel
+# Run establish a ngrok tunnel
+
+
+Sample: `test.yml`
+
+```yml
+
+name: Test
+
+on: [push]
+
+jobs:
+  testsolaris:
+    runs-on: macos-latest
+    name: Test a ngrok tunnel
+    env:
+      NGROK_TOKEN : ${{ secrets.NGROK_TOKEN }}
+    steps:
+    - uses: actions/checkout@v2
+    - name: Run establish a ngrok tunnel
+      id: test
+      uses: vmactions/ngrok-tunnel@v0.0.1
+      with:
+        protocol: tcp
+        port: 22
+
+
+
+```
+
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,136 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+const fs = require("fs");
+const path = require("path");
+const os = require("os");
+
+
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+
+async function download() {
+  NGROK_MAC = "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip"
+  NGROK_Linux = "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip"
+  NGROK_Win = "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip"
+  NGROK_BSD = "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-freebsd-amd64.zip"
+
+
+  let link = NGROK_Win;
+  if (os.platform() == "darwin") {
+    link = NGROK_MAC;
+  } else if (os.platform() == "linux") {
+    link = NGROK_Linux;
+  }
+
+
+  let workingDir = __dirname;
+  {
+    core.info("Downloading: " + link);
+    let img = await tc.downloadTool(link);
+    core.info("Downloaded file: " + img);
+    await io.mv(img, path.join(workingDir, "./ngrok.zip"));
+
+  }
+
+  await exec.exec("7za e -y " + path.join(workingDir, "./ngrok.zip") + "  -o" + workingDir);
+
+
+}
+
+async function run(token, protocol, port) {
+  let ngrok = path.join(workingDir, "./ngrok")
+  if (os.platform() == "win32") {
+    ngrok += ".exe";
+  }
+
+  await exec.exec(ngrok, ["authtoken", token]);
+
+  let log = path.join(workingDir, "./ngrok.log");
+
+  await exec.exec("sh", [], { input: `${ngrok} ${protocol} ${port} --log ${log} &` });
+
+  await sleep(5000);
+
+
+  let output = "";
+  await exec.exec("sh", [], {
+    input: `egrep -o  '${protocol}://.*ngrok.io.*' ${log} | head -1`,
+    listeners: {
+      stdout: (s) => {
+        output += s;
+        core.info(s);
+      }
+    }
+  });
+
+
+  if (protocol == "tcp") {
+    let server = output.trimStart("tcp://").split(":")[0];
+    core.info("server: " + server);
+    core.setOutput("server", server);
+
+    let port = output.trimStart("tcp://").split(":")[1];
+    core.info("port: " + port);
+    core.setOutput("port", port);
+
+  } else if (protocol == "http") {
+    let lines = output.split('//');
+    let server = lines[lines.length - 1];
+    core.info("server: " + server);
+    core.setOutput("server", server);
+  }
+
+}
+
+
+
+async function main() {
+  let token = process.env['NGROK_TOKEN'];
+  if (!token) {
+    core.setFailed("No NGROK_TOKEN !");
+    return;
+  }
+
+  let protocol = core.getInput("protocol");
+  core.info("protocol: " + protocol);
+  if (!protocol) {
+    protocol = "tcp";
+  }
+
+  let port = core.getInput("port");
+  core.info("port: " + port);
+  if (!port) {
+    core.setFailed("No port !");
+    return;
+  }
+
+
+  var envs = core.getInput("envs");
+  console.log("envs:" + envs);
+  if (envs) {
+    fs.appendFileSync(path.join(process.env["HOME"], "/.ssh/config"), "SendEnv " + envs + "\n");
+  }
+
+  var prepare = core.getInput("prepare");
+  if (prepare) {
+    core.info("Running prepare: " + prepare);
+    await exec.exec("ssh -t solaris", [], { input: prepare });
+  }
+
+
+  await download();
+
+  await run(token, protocol, port);
+
+
+}
+
+
+
+main().catch(ex => {
+  core.setFailed(ex.message);
+});
+
